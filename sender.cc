32a7d677961e091493341900e4df1b7605e39bf5
Kalpit Munot <kalpit12@server-0.test1.uwmadison744-f21.emulab.net>
2022-04-01 15:35:17 -0600

    Checksum Header

--- sender.cc
+++ sender.cc
@@ -1,1 +1,20 @@
-//Will do it after implementing Client
+#include <iostream>
+#include "SHA256.h"
+ 
+using std::string;
+using std::cout;
+using std::endl;
+ 
+int main(int argc, char *argv[])
+{
+    string A = "grape";
+	string B = "grapes";
+    //string output1 = sha256(A);
+	//string output1 = sha256(A);
+	if(sha256(A)==sha256(B))
+	cout<<"true"<<endl;
+	else
+	cout<<"False"<<endl;
+    
+    return 0;
+}

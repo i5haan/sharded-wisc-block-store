a465c4c9e9918bf42e9c67d538c2bf5f5137692f
Kalpit Munot <kalpit12@server-0.test1.uwmadison744-f21.emulab.net>
2022-04-06 17:58:04 -0600

    minor change

--- hafs/client_impl.h
+++ hafs/client_impl.h
@@ -53,9 +53,9 @@ class HafsClient {
 
         void checkHeartBeat() {
             HeartBeatResponse response = this->HeartBeat();
-             if(response.status() == 0 && (response.health() == 1 || response.health() == 2 || response.health() == 3)) {        
+            // if(response.status() == 0 && (response.health() == 1 || response.health() == 2 || response.health() == 3)) {        
                 std::cout << "[HafsCLient] Recieved Hearbeat, Status[" << HeartBeatResponse_Status_Name(response.status()) << "], Role[" << HeartBeatResponse_Role_Name(response.role()) << "], Health[" << HeartBeatResponse_Health_Name(response.health()) << "] from address[" << this->address << "]" << std::endl;
-            }
+            //}
             if(response.status() == HeartBeatResponse_Status_INVALID) {
                 replicatorHealth = HeartBeatResponse_Health_UNHEALTHY;
                 this->isAlive = false;

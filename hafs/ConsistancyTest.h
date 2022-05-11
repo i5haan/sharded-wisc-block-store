b7514a2cec91c5bb18cafd6e28d104a971e02581
sandy14795 <sandysingh4545@gmail.com>
2022-05-11 15:44:53 -0500

    changes done for same addr write time calculations.

--- hafs/ConsistancyTest.h
+++ hafs/ConsistancyTest.h
@@ -154,6 +154,104 @@ int Shard_ClientConsistencyDiffAddr(int NumWrites, string primaryAddr, string ba
     return 1;
 }
 
+
+int Shard_ClientConsistencySameAddr(int NumWrites, string primaryAddr, string backupAddr, bool flag)
+{
+    HafsClientFactory client1(primaryAddr, backupAddr);
+
+    
+    //HafsClient client1(grpc::CreateChannel("155.98.36.86:8093", grpc::InsecureChannelCredentials()), "155.98.36.86:8093", false);
+    //HafsClient client2(grpc::CreateChannel("155.98.36.88:8094", grpc::InsecureChannelCredentials()), "155.98.36.88:8094", false);
+    
+    string res;
+
+    vector<int> UsedAddr;
+    vector<double> writeTimes;
+    Timer2 time;
+
+    ofstream myfile;
+    myfile.open ("values_odd_sameaddr.txt",  ios::in | ios::app);
+
+    ofstream myfile1;
+    myfile1.open ("values_even_sameaddr.txt",  ios::in | ios::app);
+    
+    // //serving odd writes.
+    // if(flag == true)
+    // {
+        
+    //     //myfile << "Writing this to a file.\n";
+    //     //myfile.close();
+    // }
+    // else
+    // {
+    //     ofstream myfile;
+    //     myfile.open ("example_even.txt");
+
+    // }
+    
+    for(int i = 0; i < NumWrites; i++) {
+        int CharId = rand()%26;
+        string data = string(4096, 'a'+ CharId);
+        time.start();
+        if(flag == true)
+        {
+            client1.Write((2+1)*4096, data);
+        }
+        else
+        {
+            client1.Write((2)*4096, data);
+        }
+        time.stop();
+        writeTimes.push_back(time.get_time_in_nanoseconds());
+
+        if(flag == true)
+        {
+            myfile << time.get_time_in_nanoseconds()<<endl;
+        }
+        else
+        {
+            myfile1 << time.get_time_in_nanoseconds()<<endl;
+        }
+        
+        //metric1.add(time.get_time_in_nanoseconds());
+        UsedAddr.push_back(i*4096);
+        /*client.Read(i*4096, &res);
+
+        if(res!=data)
+        {
+            cout<<"Read Write Failed"<<endl;
+            return 0;
+        }*/
+    }
+    //cout<<time.get_time_in_nanoseconds()<<endl;
+    
+    // for(int i=0;i<UsedAddr.size();i++)
+    // {
+    //     if(client.CheckConsistancy(UsedAddr[i])!=true)
+    //     {         
+    //         return 0;
+    //     }
+    // }
+
+    if(flag == true)
+    {
+        myfile << accumulate(writeTimes.begin(),writeTimes.end(),0) << endl;
+    }
+    else
+    {
+        myfile1 << accumulate(writeTimes.begin(),writeTimes.end(),0) << endl;
+    }
+    //myfile << accumulate(writeTimes.begin(),writeTimes.end(),0) << endl;
+    //myfile << accumulate(writeTimes.begin(),writeTimes.end(),0) << endl;
+    
+    myfile.close();
+    myfile1.close();
+    usleep(10*1000000);
+    return 1;
+}
+
+
+
 /*
 Single Client 
 All address Same
